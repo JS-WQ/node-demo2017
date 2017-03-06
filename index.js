ecc4a88bfa8ab2e933a3011289dead984e013f59
JSliuwenqiang <552410301@qq.com>
2017-03-06 23:58:56 +0800

    second

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var fs = require('fs')
 var url = require('url')
 
 //console.log(Object.keys(http))
-var port = process.env.PORT || 8888;
+var port = process.env.PORT || 4433;
 
 var server = http.createServer(function(request, response){
 
@@ -13,8 +13,22 @@ var server = http.createServer(function(request, response){
 
   //从这里开始看，上面不要看
 
-  if(path === '/index.html'){
-    response.end('11111111')
+  if(path === '/'){
+    var string = fs.readFileSync('./index.html')
+    response.setHeader('Content-Type', 'text/html;charset=utf-8')
+    response.end(string)
+  }if(path === '/style.css'){
+    var string = fs.readFileSync('./style.css')
+    response.setHeader('Content-Type', 'text/css')
+    response.end(string)
+  }if(path === '/print-style.css'){
+    var string = fs.readFileSync('./print-style.css')
+    response.setHeader('Content-Type', 'text/css')
+    response.end(string)
+  }if(path === '/main.js'){
+    var string = fs.readFileSync('./main.js')
+    response.setHeader('Content-Type', 'application/javascript')
+    response.end(string)
   }else{
     response.end('404')
   }
@@ -23,4 +37,5 @@ var server = http.createServer(function(request, response){
 })
 
 server.listen(port)
-console.log('监听 8888 成功')
+console.log('监听 4433 成功')
+
